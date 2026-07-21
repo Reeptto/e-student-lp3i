47693ef9528b57e17b1c647864404905e5d0e094
Reeptto <0204ariaprianto@gmail.com>
2026-07-21 15:53:20 +0700

    Fix filename case for MataKuliah model

--- app/Models/MataKuliah.php
+++ app/Models/MataKuliah.php
@@ -9,23 +9,27 @@ class MataKuliah extends Model
 {
     protected $table = 'matakuliah';
     protected $fillable = [
-        'kode_mk',
         'nama_mk',
+        'kode_mk',
         'deskripsi',
+        'tipe_matakuliah',
+        'id_program_studi',
+        'semester',
+        'sks',
     ];
 
     public function tugas()
     {
-        return $this->hasMany(Tugas::class, 'mk_id');
+        return $this->hasMany(Tugas::class, 'id_mk');
     }
 
     public function nilai()
     {
-        return $this->hasMany(Nilai::class, 'mk_id');
+        return $this->hasMany(Nilai::class, 'id_mk');
     }
 
     public function materi()
     {
-        return $this->hasMany(Material::class, 'mk_id');
+        return $this->hasMany(Material::class, 'id_mk', 'id_mk');
     }
 }
